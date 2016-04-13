f60ea78f26d5c48d1123421d1e3ed4def68e0cfc
Dustin J. Mitchell <dustin@mozilla.com>
2016-04-13 15:34:22 +0000

    show help more

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ var program = require('commander');
 
 // global options
 program
-  .version(require('./package.json').version);
+  .version(require('./package.json').version)
 
 // allow each subcommand to set up
 fs.readdirSync(path.join(__dirname, 'scripts')).forEach((f) => {
   require('./scripts/' + f)(program);
 });
 
+// default to showing help
+program.command('*')
+  .description("..your command here?")
+  .action(() => program.help(txt => txt));
+
 // run
 program.parse(process.argv);
+if (!process.argv.slice(2).length) {
+  program.help((txt) => txt);
+}
