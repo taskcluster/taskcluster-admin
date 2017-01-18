b1e313ec9edc949ec8d22acd2445aec0c2808f38
Dustin J. Mitchell <dustin@mozilla.com>
2017-01-18 21:18:19 +0000

    allow make-gecko-branch-role to run in --noop mode

--- src/make-gecko-branch-role.js
+++ src/make-gecko-branch-role.js
@@ -1,18 +1,19 @@
 module.exports.setup = (program) => {
   return program
     .command('make-gecko-branch-role <path> <project> <level>')
+    .option('-n, --noop', 'Don\'t change roles, just show difference')
     .description('create or update a gecko branch role (repo:hg.mozilla.org/<path>:*)');
 };
 
-module.exports.run = function(path, project, level) {
+module.exports.run = async function(path, project, level, options) {
   var taskcluster = require('taskcluster-client');
   var chalk = require('chalk');
+  var arrayDiff = require('simple-array-diff');
   var auth = new taskcluster.Auth();
 
   var roleId = 'repo:hg.mozilla.org/' + path + ':*';
   var scopes = [
     'assume:moz-tree:level:<level>',
-    'queue:rerun-task',
     'queue:route:index.buildbot.branches.<project>.*',
     'queue:route:index.buildbot.revisions.*',
     'queue:route:index.docker.images.v1.<project>.*',
@@ -20,6 +21,8 @@ module.exports.run = function(path, project, level) {
     'queue:route:index.gecko.v2.<project>.*',
     'queue:route:tc-treeherder-stage.<project>.*',
     'queue:route:tc-treeherder.<project>.*',
+    'queue:route:tc-treeherder-stage.v2.<project>.*',
+    'queue:route:tc-treeherder.v2.<project>.*',
     'queue:route:coalesce.v1.builds.<project>.*',
   ].map((scope) =>
     scope
@@ -28,30 +31,46 @@ module.exports.run = function(path, project, level) {
 
   );
 
-  auth.role(roleId).catch((err) => {
-    if (err.statusCode == 404) {
-      return;
+  var role;
+  try {
+    role = await auth.role(roleId);
+  } catch (err) {
+    if (err.statusCode != 404) {
+      throw err;
     }
-    throw err;
-  }).then((role) => {
-    if (role) {
-      console.log(chalk.green.bold('updating role'));
-      return auth.updateRole(roleId, {
-        description: role.description,
-        scopes,
-      });
-    } else {
-      console.log(chalk.green.bold('creating role'));
-      return auth.createRole(roleId, {
-        description: 'Scopes for tasks triggered from https://hg.mozilla.org/' + path,
-        scopes,
-      });
+  }
+
+  var got_scopes = role ? role.scopes : [];
+  var diff = arrayDiff(got_scopes, scopes);
+  if (diff.removed.length || diff.added.length) {
+    console.log(chalk.green.bold(`scope changes required for role ${roleId}:`));
+    diff.removed.forEach(s => console.log(chalk.red(`- ${s}`)));
+    diff.added.forEach(s => console.log(chalk.green(`+ ${s}`)));
+  } else {
+    console.log(chalk.green.bold(`no changes required for role ${roleId}:`));
+    return;
+  }
+
+  if (!options.noop) {
+    try {
+      if (role) {
+        console.log(chalk.green.bold('updating role'));
+        await auth.updateRole(roleId, {
+          description: role.description,
+          scopes,
+        });
+      } else {
+        console.log(chalk.green.bold('creating role'));
+        await auth.createRole(roleId, {
+          description: 'Scopes for tasks triggered from https://hg.mozilla.org/' + path,
+          scopes,
+        });
+      }
+    } catch (err) {
+      console.log(err);
+      process.exit(1);
     }
-  }).then(() => {
     console.log(chalk.green.bold('done'));
-  }).catch((err) => {
-    console.log(err);
-    process.exit(1);
-  });
+  }
 };
 
