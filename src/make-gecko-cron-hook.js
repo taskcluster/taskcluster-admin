ab248fb25142e35ab41906c30dd2a2c575b27d0a
Dustin J. Mitchell <dustin@mozilla.com>
2017-01-19 21:16:47 +0000

    add make-gecko-cron-hook

---
+++ src/make-gecko-cron-hook.js
@@ -0,0 +1,162 @@
+import editRole from './util/edit-role';
+import chalk from 'chalk';
+import taskcluster from 'taskcluster-client';
+import {diffLines} from 'diff';
+
+module.exports.setup = (program) => {
+  return program
+    .command('make-gecko-cron-hook <path> <project> <level>')
+    .option('-n, --noop', 'Don\'t change roles, just show difference')
+    .description('create or update a hook and its role for running gecko cron jobs; path ' +
+                 'should be the portion after `hg.mozilla.org/`');
+};
+
+module.exports.run = async function(path, project, level, options) {
+  var taskcluster = require('taskcluster-client');
+  var chalk = require('chalk');
+
+  var hookGroupId = 'project-releng';
+  var hookId = `cron-task-${path.replace('/', '-')}`;
+
+  // set up role
+
+  await editRole({
+    roleId: `hook-id:${hookGroupId}/${hookId}`,
+    description: [
+      '*DO NOT EDIT*',
+      '',
+      `Scopes for the cron task for https://hg.mozilla.org/${path}`,
+      '',
+      'This role is configured automatically by [taskcluster-admin](https://github.com/taskcluster/taskcluster-admin).',
+    ].join('\n'),
+    scopes: [`assume:repo:hg.mozilla.org/${path}:cron:*`],
+    noop: options.noop,
+  });
+
+  // set up hook
+
+  const newHook = {
+    metadata: {
+      name: `Cron task for https://hg.mozilla.org/${path}`,
+      description: [
+        '*DO NOT EDIT*',
+        '',
+        `The cron hook for https://hg.mozilla.org/${path}`,
+        '',
+        'This hook is configured automatically by [taskcluster-admin](https://github.com/taskcluster/taskcluster-admin).',
+      ].join('\n'),
+      owner: 'mozilla-taskcluster-maintenance@mozilla.com',
+      emailOnError: true
+    },
+    task: {
+      provisionerId: 'aws-provisioner-v1',
+      workerType: 'gecko-decision',
+      schedulerId: `gecko-level-${level}`,
+      routes: [],
+      scopes: [`assume:hook-id:${hookGroupId}/${hookId}`],
+      payload: {
+        env: {
+          GECKO_BASE_REPOSITORY: 'https://hg.mozilla.org/mozilla-unified',
+          GECKO_HEAD_REPOSITORY: `https://hg.mozilla.org/${path}`,
+          GECKO_HEAD_REF: 'tip',
+          HG_STORE_PATH: '/home/worker/checkouts/hg-store'
+        },
+        cache: {}, // see below
+        features: {
+          taskclusterProxy: true,
+          chainOfTrust: true
+        },
+        image: 'taskcluster/decision:0.1.7',
+        maxRunTime: 1800,
+        command: [
+          '/home/worker/bin/run-task',
+          '--vcs-checkout=/home/worker/checkouts/gecko',
+          '--',
+          'bash',
+          '-cx',
+          [
+            'cd /home/worker/checkouts/gecko',
+            'ln -s /home/worker/artifacts artifacts',
+            './mach --log-no-times taskgraph cron --base-repository=$GECKO_BASE_REPOSITORY --head-repository=$GECKO_HEAD_REPOSITORY ' +
+              `--head-ref=$GECKO_HEAD_REF --project=${project} --level=${level}`
+          ].join(' && '),
+        ],
+        artifacts: {
+          public: {
+            type: 'directory',
+            path: '/home/worker/artifacts'
+          }
+        }
+      },
+      metadata: {
+        owner: 'mozilla-taskcluster-maintenance@mozilla.com',
+        source: `https://tools.taskcluster.net/hooks/#${hookGroupId}/${hookId}`,
+        description: `See https://tools.taskcluster.net/hooks/#${hookGroupId}/${hookId}`,
+        name: `Cron task for https://hg.mozilla.org/${path}`,
+      },
+      priority: 'normal',
+      retries: 5,
+      tags: {},
+      extra: {}
+    },
+    schedule: [
+      // TODO: enable :)
+      //"0 0,15,30,45 * * * *", // every 15 minutes
+    ],
+    deadline: '1 hour',
+    expires: '7 days'
+  };
+  // set a property that is not a valid identifier
+  newHook.task.payload.cache[`level-${level}-checkouts`] = '/home/worker/checkouts';
+
+  const hooks = new taskcluster.Hooks();
+
+  let hook;
+  try {
+    hook = await hooks.hook(hookGroupId, hookId);
+    delete hook['hookId'];
+    delete hook['hookGroupId'];
+  } catch (err) {
+    if (err.statusCode !== 404) {
+      throw err;
+    }
+    hook = {};
+  }
+
+  // compare and display the differences
+  const diffs = diffLines(
+      JSON.stringify(hook, null, 2),
+      JSON.stringify(newHook, null, 2),
+      {newlineIsToken: true});
+  let diffsFound = false;
+  diffs.forEach(diff => {
+    if (diff.added || diff.removed) {
+      diffsFound = true;
+    }
+  });
+
+  if (diffsFound) {
+    console.log(chalk.green.bold(`changes required for hook ${hookGroupId}/${hookId}:`));
+    diffs.forEach(diff => {
+      if (diff.added) {
+        diff.value.split(/\n/).forEach(l => console.log(chalk.green('+' + l)));
+      } else if (diff.removed) {
+        diff.value.split(/\n/).forEach(l => console.log(chalk.red('-' + l)));
+      } else {
+        diff.value.split(/\n/).forEach(l => console.log(' ' + l));
+      }
+    });
+  } else {
+    console.log(chalk.green.bold(`no changes required for hook ${hookGroupId}/${hookId}`));
+  }
+
+  if (!options.noop && diffsFound) {
+    if (hook.task) {
+      console.log(chalk.green.bold('updating hook'));
+      await hooks.updateHook(hookGroupId, hookId, newHook);
+    } else {
+      console.log(chalk.green.bold('creating hook'));
+      await hooks.createHook(hookGroupId, hookId, newHook);
+    }
+  }
+};
