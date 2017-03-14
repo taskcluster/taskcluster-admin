0685728f9310834acd28894f1bf9746827952730
Dustin J. Mitchell <dustin@mozilla.com>
2017-03-14 20:54:44 +0000

    call the awsProvisioner.terminateAllInstancesOfWorkerType endpoint (note that it does not seem to work)

---
+++ src/kill-all-workers.js
@@ -0,0 +1,26 @@
+import _ from 'lodash';
+
+module.exports.setup = (program) => {
+  return program
+    .command('kill-all-workers [workerTypes...]')
+    .description('Terminate all instances of the given workerType')
+    .option('--all', 'Operate on all workerTypes');
+};
+
+module.exports.run = async function(workerTypesOption, options) {
+  var taskcluster = require('taskcluster-client');
+  var chalk = require('chalk');
+  var awsProvisioner = new taskcluster.AwsProvisioner();
+  var workerTypes = workerTypesOption;
+
+  if (options.all) {
+    workerTypes = await awsProvisioner.listWorkerTypes();
+  }
+
+  while (workerTypes.length) {
+    const workerType = workerTypes.pop();
+    console.log(chalk.yellow.bold(`terminating instances of ${workerType}`));
+    await awsProvisioner.terminateAllInstancesOfWorkerType(workerType);
+    console.log(chalk.yellow('done'));
+  }
+};
