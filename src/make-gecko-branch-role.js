d04c08035efc84c4bb75dfc1d9877ca019046c17
Dustin J. Mitchell <dustin@mozilla.com>
2017-03-16 16:58:05 +0000

    add index.insert-task:.. routes, and allow --all with make-gecko-branch-role

--- src/make-gecko-branch-role.js
+++ src/make-gecko-branch-role.js
@@ -3,94 +3,108 @@ import {getProjects, hgmoPath, scmLevel} from './util/projects';
 
 module.exports.setup = (program) => {
   return program
-    .command('make-gecko-branch-role <project>')
+    .command('make-gecko-branch-role [projects...]')
     .option('-n, --noop', 'Don\'t change roles, just show difference')
+    .option('--all', 'Operate on all projects')
     .description('create or update a gecko branch role');
 };
 
-module.exports.run = async function(projectName, options) {
+module.exports.run = async function(projectsOption, options) {
   var taskcluster = require('taskcluster-client');
   var chalk = require('chalk');
   var arrayDiff = require('simple-array-diff');
   var projects = await getProjects();
 
-  var project = projects[projectName];
-  if (!project) {
-    console.log(chalk.red(`Project ${projectName} is not defined in production-branches.json`));
-    process.exit(1);
+  if (options.all) {
+    projectsOption = Object.keys(projects);
   }
 
-  var level = scmLevel(project);
-  if (!level) {
-    console.log(chalk.red(`Cannot determine project level`));
-    process.exit(1);
+  while (projectsOption.length) {
+    var projectName = projectsOption.pop();
+    var project = projects[projectName];
+    if (!project) {
+      console.log(chalk.red(`Project ${projectName} is not defined in production-branches.json`));
+      process.exit(1);
+    }
+
+    var level = scmLevel(project);
+    if (!level) {
+      console.log(chalk.red(`Cannot determine project level`));
+      process.exit(1);
+    }
+
+    var path = hgmoPath(project);
+    if (!path) {
+      console.log(chalk.red(`Unrecognized project repository ${project.repo}`));
+      process.exit(1);
+    }
+
+    var roleId = `repo:hg.mozilla.org/${path}:*`;
+    var scopes = [
+      'assume:moz-tree:level:<level>',
+      'queue:route:index.buildbot.branches.<project>.*',
+      'index:insert-task:buildbot.branches.<project>.*',
+      'queue:route:index.buildbot.revisions.*',
+      'index:insert-task:buildbot.revisions.*',
+      'queue:route:index.docker.images.v1.<project>.*',
+      'index:insert-task:docker.images.v1.<project>.*',
+      'queue:route:index.gecko.v2.<project>.*',
+      'index:insert-task:gecko.v2.<project>.*',
+      'queue:route:tc-treeherder-stage.<project>.*',
+      'queue:route:tc-treeherder.<project>.*',
+      'queue:route:tc-treeherder-stage.v2.<project>.*',
+      'queue:route:tc-treeherder.v2.<project>.*',
+      'queue:route:coalesce.v1.builds.<project>.*',
+    ].map((scope) =>
+      scope
+      .replace('<project>', projectName)
+      .replace('<level>', level)
+
+    );
+
+    var description = [
+      '*DO NOT EDIT*',
+      '',
+      `Scopes for tasks triggered from pushes to https://hg.mozilla.org/${path}`,
+      '',
+      'This role is configured automatically by [taskcluster-admin](https://github.com/taskcluster/taskcluster-admin).',
+    ].join('\n');
+
+    await editRole({
+      roleId,
+      description,
+      scopes,
+      noop: options.noop,
+    });
+
+    // nightly-specific scopes
+
+    if (project.features['taskcluster-cron']) {
+      roleId = `repo:hg.mozilla.org/${path}:cron:nightly-*`;
+      scopes = [
+        'assume:project:releng:nightly:level-<level>:<project>',
+      ].map((scope) =>
+        scope
+        .replace('<project>', projectName)
+        .replace('<level>', level)
+
+      );
+
+      description = [
+        '*DO NOT EDIT*',
+        '',
+        `Scopes for nighlty cron tasks triggered from pushes to https://hg.mozilla.org/${path}`,
+        '',
+        'This role is configured automatically by [taskcluster-admin](https://github.com/taskcluster/taskcluster-admin).',
+      ].join('\n');
+
+      // edit the nightly-specific role
+      await editRole({
+        roleId,
+        description,
+        scopes,
+        noop: options.noop,
+      });
+    }
   }
-
-  var path = hgmoPath(project);
-  if (!path) {
-    console.log(chalk.red(`Unrecognized project repository ${project.repo}`));
-    process.exit(1);
-  }
-
-  var roleId = `repo:hg.mozilla.org/${path}:*`;
-  var scopes = [
-    'assume:moz-tree:level:<level>',
-    'queue:route:index.buildbot.branches.<project>.*',
-    'queue:route:index.buildbot.revisions.*',
-    'queue:route:index.docker.images.v1.<project>.*',
-    'queue:route:index.gecko.v2.<project>.*',
-    'queue:route:tc-treeherder-stage.<project>.*',
-    'queue:route:tc-treeherder.<project>.*',
-    'queue:route:tc-treeherder-stage.v2.<project>.*',
-    'queue:route:tc-treeherder.v2.<project>.*',
-    'queue:route:coalesce.v1.builds.<project>.*',
-  ].map((scope) =>
-    scope
-    .replace('<project>', projectName)
-    .replace('<level>', level)
-
-  );
-
-  var description = [
-    '*DO NOT EDIT*',
-    '',
-    `Scopes for tasks triggered from pushes to https://hg.mozilla.org/${path}`,
-    '',
-    'This role is configured automatically by [taskcluster-admin](https://github.com/taskcluster/taskcluster-admin).',
-  ].join('\n');
-
-  await editRole({
-    roleId,
-    description,
-    scopes,
-    noop: options.noop,
-  });
-
-  // nightly-specific scopes
-
-  roleId = `repo:hg.mozilla.org/${path}:cron:nightly-*`;
-  scopes = [
-    'assume:project:releng:nightly:level-<level>:<project>',
-  ].map((scope) =>
-    scope
-    .replace('<project>', projectName)
-    .replace('<level>', level)
-
-  );
-
-  description = [
-    '*DO NOT EDIT*',
-    '',
-    `Scopes for nighlty cron tasks triggered from pushes to https://hg.mozilla.org/${path}`,
-    '',
-    'This role is configured automatically by [taskcluster-admin](https://github.com/taskcluster/taskcluster-admin).',
-  ].join('\n');
-
-  // edit the nightly-specific role
-  await editRole({
-    roleId,
-    description,
-    scopes,
-    noop: options.noop,
-  });
 };
